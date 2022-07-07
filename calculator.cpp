089ad73d4fba30b9cb9882bbf8846b16fb174824
Pravallika Gajul <pravsgajul@gmail.com>
2022-07-07 17:56:33 +0530

    calculator

---
+++ calculator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b){
+    int sum=a+b;
+    return sum;
+}
+
+int difference(int a,int b){
+    int diff=a-b;
+    return diff;
+}
+
+int main(){
+
+}
