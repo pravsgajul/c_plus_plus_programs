47635c5a5efd7980aa14f4b33fda9a49e77d3c5f
Pravallika Gajul <pravsgajul@gmail.com>
2022-07-08 11:06:05 +0530

    Commit

--- calculator.cpp
+++ calculator.cpp
@@ -11,6 +11,21 @@ int difference(int a,int b){
     return diff;
 }
 
-int main(){
+int product(int a,int b){
+    int prod=a*b;
+    return prod;
+}
 
+int division(int a,int b){
+    int div=a/b;
+    return div;
+}
+
+int main(){
+    int num1;
+    string operation;
+    cout<<"Enter the expression";
+    cin>>num1;
+    cin>> operation;
+    cout<< operation;
 }
