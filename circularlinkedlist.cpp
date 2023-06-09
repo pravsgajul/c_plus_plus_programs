17c032d54d2756ce20d3c19cfdb1e5517e2a3263
Pravallika Gajul <pravsgajul@gmail.com>
2023-06-09 11:22:15 +0530

    changes

--- circularlinkedlist.cpp
+++ circularlinkedlist.cpp
@@ -14,26 +14,35 @@ class node{
 
 void insertatHead(node* &head,int val){
 	node* n= new node(val);
+	node* i=head;
+	node* j= head;
+	
 	if(head==NULL){
 		head=n;
-		cout<<head->data;
+		cout<<"head" <<head->data;
 	}
-	node* i=head;
-	if (head!=NULL){
+	cout<<j->data;
+	/*while(j->next!=i){
+		cout<<"inside loop";
+		j=j->next;
 		n->next=i;
 		head=n;
-		i->next=head;
 		cout<<head->data;
 	}
+	j->next=n;*/
+}
+
+void insertbetween(node* head,int val1,int val2, int newval){
+	node* n= new node(newval);
 }
 
 void displaylist(node* head){
 	node* i=head;
-	while(i->next!=NULL){
+	while(i->next!=i){
 		cout<<i->data<<"->";
 		i=i->next;
 	}
-	cout<<"NULL";
+	
 }
 
 int main(){
@@ -42,6 +51,13 @@ int main(){
 	insertatHead(head,12);
 	insertatHead(head,34);
 	insertatHead(head,45);
+	insertatHead(head,233);
+	insertatHead(head,78);
+	insertatHead(head,66);
+	insertatHead(head,97);
+	insertatHead(head,90);
+	insertatHead(head,22);
+
 	displaylist(head);
 	
 }
