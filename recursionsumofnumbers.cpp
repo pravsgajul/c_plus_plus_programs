cbd619a35cc324e9ffdf3b09ab28335fd24f8187
Pravallika Gajul <pravsgajul@gmail.com>
2023-10-04 09:54:59 +0530

    Edit

---
+++ recursionsumofnumbers.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int sumofn(int m){
+	if (m<=0){
+		return 0;
+	}
+	else{
+		return m+sumofn(m-1);
+	}
+}
+
+int main(){
+	int n,p;
+	cin >> n;
+	p=sumofn(n);
+	cout<<p;
+}
